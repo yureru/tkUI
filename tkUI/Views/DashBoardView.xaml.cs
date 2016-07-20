062276b137b0f04b78c9604e2ec123e71d654369
roli <roli@cock.li>
2016-07-20 18:35:46 -0500

    Revert "dun goofed"
    This reverts commit 41c8af1af4cd157360e2312a981cc98f2af1a2b2.

--- tkUI/Views/DashBoardView.xaml.cs
+++ tkUI/Views/DashBoardView.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using tkUI.Subpages.GraphQuickBoxes.ViewModels;
-using tkUI.Subpages.GraphQuickBoxes.Utils;
 
 namespace tkUI.Views
 {
@@ -23,28 +22,11 @@ namespace tkUI.Views
     /// </summary>
     public partial class DashBoardView : UserControl
     {
-        static PageFromQuickBoxes currentVM;
-        static GraphComboboxes Comboboxes;
-
         public DashBoardView()
         {
             InitializeComponent();
             /*ExpectedPaymentViewModel context = new ExpectedPaymentViewModel();
             this.DataContext = context;*/
-            ComboBox item = new ComboBox();
-            string[] labels = new string[] { "First", "second", "Third" };
-            item.ItemsSource = labels;
-            //Wrapper = item;
-            Grid.SetRow(item, 0);
-            Main.Children.Add(item);
-            // I think it would be bette create a MMVM for the Comboboxes itself, a ContentControl
-            // that changes when the view is changed.
-            currentVM = CurrentGraph.Content as PageFromQuickBoxes;
         }
-
-        ///TODO: 1. Check if we're mapping correctly the ViewModels with the comboboxes // Sems Ok
-        /// 2. Need a command to bind the action of the combobox with the corresponding chart
-        /// 3. Check if when we change the QuickBox the combobox is changed too.
-        /// 4. And also when changing from the navigation menu.
     }
 }
