54f41b534df3dcd0b9abb9490f614d6342324174
roli <roli@cock.li>
2016-07-22 01:53:59 -0500

    Started implementing the MMVM for the CRUD

---
+++ tkUI/Subpages/EmployeesCRUD/Utils/PageFromCRUD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tkUI.Helper_Classes;
+
+namespace tkUI.Subpages.EmployeesCRUD.Utils
+{
+    abstract class PageFromCRUD : IButton, IRadioButtonChecked
+    {
+
+        #region Fields
+
+        bool _isChecked;
+
+        #endregion // Fields
+
+        #region Interfaces
+
+        public bool Checked
+        {
+            get { return _isChecked; }
+            set { _isChecked = value; }
+        }
+
+        public abstract string Name { get; set; }
+
+        #endregion // Interfaces
+    }
+}
---
+++ tkUI/Subpages/EmployeesCRUD/ViewModels/AddAdminViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tkUI.Subpages.EmployeesCRUD.ViewModel
+{
+    class AddAdminViewModel
+    {
+    }
+}
