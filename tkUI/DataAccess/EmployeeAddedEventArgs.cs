408c9c1a347cfe44e412b8c6fbb17c6b05069d91
roli <roli@cock.li>
2016-07-31 12:56:20 -0500

    Class to handle the loading of employees.
    Going to use a DB later.

---
+++ tkUI/DataAccess/EmployeeAddedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tkUI.Models;
+
+namespace tkUI.DataAccess
+{
+    /// <summary>
+    /// Event arguments used by CustomerRepository's CustomerAdded event.
+    /// </summary>
+    class EmployeeAddedEventArgs : EventArgs
+    {
+        public EmployeeAddedEventArgs(Employee newEmployee)
+        {
+            this.NewEmployee = newEmployee;
+        }
+
+        public Employee NewEmployee { get; private set; }
+    }
+}
---
+++ tkUI/DataAccess/EmployeeRepository.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Windows;
+using System.Windows.Resources;
+
+using tkUI.Models;
+
+namespace tkUI.DataAccess
+{
+    /// <summary>
+    /// Represents a source of employees in the application.
+    /// </summary>
+    class EmployeeRepository
+    {
+
+        #region Fields
+
+        readonly List<Employee> _employees;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new repository of employees.
+        /// </summary>
+        /// <param name="employeeDataFile"></param>
+        public EmployeeRepository(string employeeDataFile)
+        {
+            _employees = LoadEmployees(employeeDataFile);
+        }
+
+        #endregion // Constructors
+
+        #region Public Interface
+
+        /// <summary>
+        /// Raised when a employee is placed into the repository.
+        /// </summary>
+        public event EventHandler<EmployeeAddedEventArgs> EmployeeAdded;
+
+        /// <summary>
+        /// Places the specified customer into the repository.
+        /// If the customer is already in the repository, an
+        /// exception is not thrown.
+        /// </summary>
+        /// <param name="employee"></param>
+        public void AddEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                throw new ArgumentNullException("employee");
+            }
+
+            if (!_employees.Contains(employee))
+            {
+                _employees.Add(employee);
+
+                if (this.EmployeeAdded != null)
+                {
+                    this.EmployeeAdded(this, new EmployeeAddedEventArgs(employee));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns true if the specified employee exists in the
+        /// repository, or false if it is not.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public bool ContainsEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                throw new ArgumentNullException("employee");
+            }
+
+            return _employees.Contains(employee);
+        }
+
+        /// <summary>
+        /// Returns a shallow-copied list of all employees in the repository.
+        /// </summary>
+        /// <returns></returns>
+        public List<Employee> GetEmployees()
+        {
+            return new List<Employee>(_employees);
+        }
+
+        #endregion // Public Interface
+
+        #region Private Helpers
+
+        static List<Employee> LoadEmployees(string employeeDataFile)
+        {
+            ///TODO: Use a DB.
+            using (Stream stream = GetResourceStream(employeeDataFile))
+            using (XmlReader xmlRdr = new XmlTextReader(stream))
+                return (from employeeElem in XDocument.Load(xmlRdr).Element("employees").Elements("employee")
+                        select Employee.CreateEmployee(
+                            (string)employeeElem.Attribute("FirstName"),
+                            (string)employeeElem.Attribute("LastName"),
+                            (bool)employeeElem.Attribute("Gender"))).ToList();
+        }
+
+        static Stream GetResourceStream(string resourceFile)
+        {
+            Uri uri = new Uri(resourceFile, UriKind.RelativeOrAbsolute);
+
+            StreamResourceInfo info = Application.GetResourceStream(uri);
+            if (info == null || info.Stream == null)
+            {
+                throw new ApplicationException("Missing resource file " + resourceFile);
+            }
+
+            return info.Stream;
+        }
+
+        #endregion // Private Helpers
+
+    }
+}
