3b240d66b71d8d46587240d2392ee8962a801cb1
roli <roli@cock.li>
2016-07-30 20:27:23 -0500

    Added Employee class.
    Basic implementation of model (going to improve this), and the basic
    layout for the Add Employee form.

---
+++ tkUI/Models/Employee.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace tkUI.Models
+{
+    /// <summary>
+    /// Represents an Employee of the company. This class
+    /// has built-in validation logic.
+    /// </summary>
+    class Employee : IDataErrorInfo
+    {
+
+        #region Fields
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        bool Gender { get; set; }
+
+
+        #endregion // Fields
+
+        #region Creation
+
+        public static Employee CreateNewEmployee()
+        {
+            return new Employee();
+        }
+
+        public static Employee CreateEmployee(string firstName, string lastName, bool gender)
+        {
+            return new Employee
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Gender = gender
+            };
+        }
+
+        protected Employee()
+        {
+        }
+
+        #endregion // Creation
+
+
+        #region Interface Implementations
+
+        string IDataErrorInfo.this[string propertyName]
+        {
+            get
+            {
+                return this.GetValidationError(propertyName);
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        #endregion // Interface Implementations
+
+        #region Validation
+
+        /// <summary>
+        /// Returns true if this object has no validation errors.
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                foreach (string property in ValidatedProperties)
+                {
+                    if (GetValidationError(property) != null)
+                        {
+                            return false;
+                        }
+                }
+
+                return true;
+            }
+        }
+
+        static readonly string[] ValidatedProperties =
+        {
+            "FirstName",
+            "LastName"
+        };
+
+        string GetValidationError(string propertyName)
+        {
+            if (Array.IndexOf(ValidatedProperties, propertyName) < 0)
+            {
+                return null;
+            }
+
+            string error = null;
+
+            switch (propertyName)
+            {
+                case "FirstName":
+                    error = this.ValidateFirstName();
+                    break;
+                case "LastName":
+                    error = this.ValidateLastName();
+                    break;
+                default:
+                    Debug.Fail("Unexpected property being validated on Customer: " + propertyName);
+                    break;
+            }
+
+            return error;
+        }
+
+        string ValidateFirstName()
+        {
+            if (IsStringMissing(this.FirstName))
+            {
+                return "Ingrese nombre";
+            }
+            return null;
+        }
+
+        string ValidateLastName()
+        {
+            if (IsStringMissing(this.LastName))
+            {
+                return "Ingrese apellido";
+            }
+            return null;
+        }
+
+        static bool IsStringMissing(string value)
+        {
+            return String.IsNullOrEmpty(value) ||
+                value.Trim() == String.Empty;
+        }
+
+        #endregion
+
+    }
+}
