ee757bd57af8eaf81e3959038fd644524da228d1
roli <roli@cock.li>
2016-07-30 21:42:08 -0500

    Started the imp. of Employee Wrapper
    Used to provide a friendly UI exposing.

---
+++ tkUI/Subpages/EmployeesCRUD/ViewModels/EmployeeWrapperViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Windows.Input;
+
+using tkUI.Helper_Classes;
+using tkUI.Models;
+
+namespace tkUI.Subpages.EmployeesCRUD.ViewModels
+{
+    class EmployeeWrapperViewModel : ObservableObject, IDataErrorInfo
+    {
+
+        #region Fields
+
+        readonly Employee _employee;
+        
+
+        #endregion // Fields
+
+        #region Interface Implementations
+
+        string IDataErrorInfo.Error
+        {
+            get
+            {
+                return null;
+                //return (_employee as IDataErrorInfo).Error;
+            }
+        }
+
+        // Used to validate an unselected Combobox.
+        ///TODO: Validate Gender combobox here
+        string IDataErrorInfo.this[string propertyName]
+        {
+            get
+            {
+                return null;
+                //throw new NotImplementedException();
+            }
+        }
+
+        #endregion // Interface Implementations
+
+    }
+}
