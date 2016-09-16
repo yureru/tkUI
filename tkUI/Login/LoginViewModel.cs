cbf29117379eae38bbbab39976a37dc27cd7f042
roli <roli@cock.li>
2016-09-16 15:29:58 -0500

    Command for the Login button.

--- tkUI/Login/LoginViewModel.cs
+++ tkUI/Login/LoginViewModel.cs
@@ -4,9 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows.Input;
+using tkUI.Helper_Classes;
+
 namespace tkUI.Login
 {
-    class LoginViewModel
+    class LoginViewModel : ObservableObject
     {
+
+        #region Fields
+
+        RelayCommand _loginCommand;
+
+        #endregion // Fields
+
+
+        #region Commands
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                if (_loginCommand == null)
+                {
+                    _loginCommand = new RelayCommand(
+                        param => this.Login(),
+                        param => this.CanLogin()
+                        );
+                }
+                return _loginCommand;
+            }
+
+        }
+
+        #endregion // Commands
+
+        #region Private Helpers
+
+        void Login()
+        {
+            tkUI.App.RunAppAfterSuccessfulLogin();
+        }
+
+        bool CanLogin()
+        {
+            // TODO: Mail and Password fields are filled?
+            return true;
+        }
+
+        #endregion // Private Helpers
+
     }
 }
