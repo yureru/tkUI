e598af6288e5614c83d9936780f62a6f62c22dec
roli <roli@cock.li>
2016-09-15 23:48:06 -0500

    Constraining login window size.

--- tkUI/App.xaml.cs
+++ tkUI/App.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using tkUI.Login;
+
 namespace tkUI
 {
     /// <summary>
@@ -16,12 +18,22 @@ namespace tkUI
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            // General
+            /* TODO: Do the following tasks for Login
+             * 1- The first window to spawn is gonna be the login.
+             * 2- After the users enters the correct credentials (email and pasword) close the login window, make spawn the application
+             * window, and make that new window the main one. 
+             */
+            Window app = new LoginView();
+            LoginViewModel context = new LoginViewModel();
+            app.DataContext = context;
+            app.Show();
 
-            ApplicationView app = new ApplicationView();
+            /*ApplicationView app = new ApplicationView();
             string path = "Data/employees.xml";
             ApplicationViewModel context = new ApplicationViewModel(path);
             app.DataContext = context;
-            app.Show();
+            app.Show();*/
         }
     }
 }
