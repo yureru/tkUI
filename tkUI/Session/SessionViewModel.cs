676ea9dca9ec42aa8964138a1632c5d89cab7517
roli <roli@cock.li>
2016-09-19 01:42:51 -0500

    Basic implementation for Session navigation.

--- tkUI/Session/SessionViewModel.cs
+++ tkUI/Session/SessionViewModel.cs
@@ -4,9 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows.Input;
+using tkUI.Helper_Classes;
+
 namespace tkUI.Session
 {
-    class SessionViewModel
+    class SessionViewModel : ObservableObject
     {
+
+        // TODO: Abstract (DRY) the functionality needed to implement navigation in a MVVM way.
+
+        #region Fields
+
+        ICommand _changePageCommand;
+        IPageViewModel _currentPageViewModel;
+        List<IPageViewModel> _pageViewModels;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        public SessionViewModel()
+        {
+
+        }
+
+        #endregion // Constructors
+
+        #region Properties / Commands
+
+        public ICommand ChangePageCommand
+        {
+            get
+            {
+                if (_changePageCommand == null)
+                {
+                    _changePageCommand = new RelayCommand(
+                        p => ChangeViewModel((IPageViewModel)p),
+                        p => p is IPageViewModel
+                        );
+                }
+
+                return _changePageCommand;
+            }
+        }
+
+        public List<IPageViewModel> PageViewModels
+        {
+            get
+            {
+                if (_pageViewModels == null)
+                {
+                    _pageViewModels = new List<IPageViewModel>();
+                }
+                return _pageViewModels;
+            }
+        }
+
+        public IPageViewModel CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+
+            set
+            {
+                if (_currentPageViewModel != value)
+                {
+                    _currentPageViewModel = value;
+                    OnPropertyChanged("CurrentPageViewModel");
+                }
+            }
+        }
+
+        #endregion // Properties Commands
+
+        #region Methods
+
+        private void ChangeViewModel(IPageViewModel viewModel)
+        {
+            if (!PageViewModels.Contains(viewModel))
+            {
+                PageViewModels.Add(viewModel);
+            }
+
+            CurrentPageViewModel = PageViewModels
+                .FirstOrDefault(vm => vm == viewModel);
+        }
+
+        #endregion // Methods
+
     }
 }
