952f717a7ea34bb4ef73f5b677f6aafd1a0ad87c
roli <roli@cock.li>
2016-09-20 00:50:06 -0500

    Basic command to ForgotPassword.
    To request a remainder of the password.

--- tkUI/Session/ViewModels/ForgotPasswordViewModel.cs
+++ tkUI/Session/ViewModels/ForgotPasswordViewModel.cs
@@ -4,12 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows.Input;
+
 using tkUI.Helper_Classes;
 
 namespace tkUI.Session.ViewModels
 {
     class ForgotPasswordViewModel : IPageViewModel
     {
+
+        RelayCommand _requestRemainderCommand;
+
+        #region Commands
+
+        public ICommand RequestRemainderCommand
+        {
+            get
+            {
+                if (_requestRemainderCommand == null)
+                {
+                    _requestRemainderCommand = new RelayCommand(
+                        param => RequestRemainder(),
+                        param => CanRequestRemainder()
+                        );
+                }
+
+                return _requestRemainderCommand;
+            }
+        }
+
+        #endregion // Commands
+
+        #region Private Helpers
+
+        void RequestRemainder()
+        {
+            throw new NotImplementedException("RequestRemainder()");
+        }
+
+        bool CanRequestRemainder()
+        {
+            return true;
+        }
+
+        #endregion // Private Helpers
 
         #region Interface Implementations
 
